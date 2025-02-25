ee768b485369fbe77603e1af4a92b56e8717c2bd
shruti <080bct082.shruti@pcampus.edu.np>
2025-02-25 23:42:29 +0545

    add more features in model

--- src/app.py
+++ src/app.py
@@ -12,12 +12,15 @@ st.title('Battery Drain Prediction')
 screen_on_time = st.number_input('Enter Screen On Time (hours per day)', min_value=0.0, max_value=24.0, value=5.0)
 app_usage_time = st.number_input('Enter App Usage Time (minutes per day)', min_value=0, value=120)
 data_usage = st.number_input('Enter Data Usage (MB per day)', min_value=0, value=500)
+number_of_apps = st.number_input('Enter Number of Apps', min_value=0, value=30)
+os_type = st.selectbox('Select OS Type', ['Android', 'iOS'])
+os_type = 1 if os_type == 'iOS' else 0
 
 # Predict button
 if st.button('Predict Battery Drain'):
     # Prepare the input data as a DataFrame (model expects a 2D array)
-    input_data = pd.DataFrame([[screen_on_time, app_usage_time, data_usage]],
-                              columns=['Screen On Time (hours/day)', 'App Usage Time (min/day)', 'Data Usage (MB/day)'])
+    input_data = pd.DataFrame([[screen_on_time, app_usage_time, data_usage, number_of_apps, os_type]],
+                              columns=['Screen On Time (hours/day)', 'App Usage Time (min/day)', 'Data Usage (MB/day)', 'Number of Apps Installed', 'Operating System'])
 
     # Make a prediction
     predicted_battery_drain = model.predict(input_data)
